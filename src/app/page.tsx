aee2fce2f19147e2e65cf79d1d3a2c1e6833a1d1
fabiopadoin <fabio.padoin@gmail.com>
2025-07-16 18:57:50 +0200

    fix: hide typing challenge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import Contact from "@/components/sections/contact";
 import Hero from "@/components/sections/hero";
 import Projects from "@/components/sections/projects";
-import { TypingChallenge } from "@/components/ui/typing-challenge";
 
 export default function Home() {
   return (
     <>
-      <div className="mt-24 md:mt-28 lg:mt-32 2xl:mt-36 scroll-mt-30 lg:scroll-mt-40 items-center flex justify-center">
-        <TypingChallenge/>
-
-      </div>
       <Hero />
       <Projects />
       <Contact />
